fafe878540cb506ac13c5346786feaac352a7fb7
Zenha123 <fathimazenha21@gmail.com>
2025-07-10 06:54:51 +0530

    time added

--- base.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const sidebarToggle = document.getElementById("sidebarToggle");
-  const wrapper = document.getElementById("wrapper");
-
-  sidebarToggle.addEventListener("click", function () {
-    wrapper.classList.toggle("toggled");
-  });
-});
